be67890ceaeb4ce983b06190e4ff42422646cd07
Guru12e <guruvijaym21ec@psnacet.edu.in>
2025-10-09 15:50:39 +0530

    Update

--- components/Section1.jsx
+++ components/Section1.jsx
@@ -7,7 +7,7 @@ const Section1 = () => {
     <div className="w-screen overflow-x-hidden mx-auto min-h-screen bg-black flex flex-col pt-10">
       <div className="w-full md:w-[85%] flex-1 mx-auto justify-center object-cover relative">
         <div className="px-6 absolute z-20 h-full flex flex-col md:flex-row gap-5 justify-between items-center">
-          <div className="flex-[60%] w-full h-full flex flex-col p-5 capitalize justify-center items-center md:items-start gap-8">
+          <div className="w-full md:w-[60%] h-full flex flex-col p-5 capitalize justify-center items-center md:items-start gap-8">
             <h2 className="text-white text-center md:text-start text-4xl md:text-6xl font-extrabold">
               Connect. Learn. Grow. Together.
             </h2>
